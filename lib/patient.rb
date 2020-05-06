91f303010696ecbf7d32541f46eba592a1c57648
Saida Davis <saihdavis@gmail.com>
2020-05-06 01:51:53 +0000

    Automatically backed up by Learn

---
+++ lib/patient.rb
@@ -0,0 +1,13 @@
+class Patient
+  
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name
+  end
+  
+  def new_appointment(date, doctor)
+    Appointment.new(date, doctor, self)
+  end
+  
+end
